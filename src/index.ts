c1a83f2b1c4d0d4d7625101b5e7256d15ebeb510
Nikki Strømsnes <hello@tinybits.dk>
2022-12-06 17:00:35 +0100

    Add solid icons

---
+++ src/index.ts
@@ -0,0 +1,429 @@
+import Add from './keyline/Add.js'
+import Alert from './keyline/Alert.js'
+import Algorithm from './keyline/Algorithm.js'
+import Ambassadors from './keyline/Ambassadors.js'
+import ArchiveNode from './keyline/ArchiveNode.js'
+import Article from './keyline/Article.js'
+import Auctions from './keyline/Auctions.js'
+import Authority from './keyline/Authority.js'
+import BackUp from './keyline/BackUp.js'
+import BatchTransaction from './keyline/BatchTransaction.js'
+import Blockchain from './keyline/Blockchain.js'
+import BlogPost from './keyline/BlogPost.js'
+import BootNode from './keyline/BootNode.js'
+import Bridge from './keyline/Bridge.js'
+import Build from './keyline/Build.js'
+import Cex from './keyline/Cex.js'
+import Career from './keyline/Career.js'
+import Code from './keyline/Code.js'
+import Community from './keyline/Community.js'
+import ConnectionViaLightNode from './keyline/ConnectionViaLightNode.js'
+import ConnectionViaRemoteNode from './keyline/ConnectionViaRemoteNode.js'
+import Consensus from './keyline/Consensus.js'
+import Contract from './keyline/Contract.js'
+import Copy from './keyline/Copy.js'
+import Cryptocurrency from './keyline/Cryptocurrency.js'
+import Dex from './keyline/Dex.js'
+import DeFi from './keyline/DeFi.js'
+import Ecosystem from './keyline/Ecosystem.js'
+import Encryption from './keyline/Encryption.js'
+import Events from './keyline/Events.js'
+import Failure from './keyline/Failure.js'
+import FullNode from './keyline/FullNode.js'
+import Gaming from './keyline/Gaming.js'
+import Gift from './keyline/Gift.js'
+import GlossaryV1 from './keyline/GlossaryV1.js'
+import GlossaryV2 from './keyline/GlossaryV2.js'
+import Governance from './keyline/Governance.js'
+import Governments from './keyline/Governments.js'
+import Hackathon from './keyline/Hackathon.js'
+import Infrastructure from './keyline/Infrastructure.js'
+import Interconnected from './keyline/Interconnected.js'
+import Investors from './keyline/Investors.js'
+import Layer from './keyline/Layer.js'
+import LayerFour from './keyline/LayerFour.js'
+import LayerOne from './keyline/LayerOne.js'
+import LayerThree from './keyline/LayerThree.js'
+import LayerTwo from './keyline/LayerTwo.js'
+import Learn from './keyline/Learn.js'
+import Library from './keyline/Library.js'
+import LightNode from './keyline/LightNode.js'
+import Maintain from './keyline/Maintain.js'
+import Message from './keyline/Message.js'
+import Metaverse from './keyline/Metaverse.js'
+import Monitor from './keyline/Monitor.js'
+import Multisign from './keyline/Multisign.js'
+import Nft from './keyline/Nft.js'
+import Node from './keyline/Node.js'
+import Nominating from './keyline/Nominating.js'
+import OracleV1 from './keyline/OracleV1.js'
+import OracleV2 from './keyline/OracleV2.js'
+import Picture from './keyline/Picture.js'
+import Podcasts from './keyline/Podcasts.js'
+import PrivateKey from './keyline/PrivateKey.js'
+import ProofOfStake from './keyline/ProofOfStake.js'
+import ProofOfWork from './keyline/ProofOfWork.js'
+import Proposal from './keyline/Proposal.js'
+import Proxy from './keyline/Proxy.js'
+import PublicKey from './keyline/PublicKey.js'
+import Randomness from './keyline/Randomness.js'
+import Recovery from './keyline/Recovery.js'
+import RemoteNode from './keyline/RemoteNode.js'
+import Remove from './keyline/Remove.js'
+import Report from './keyline/Report.js'
+import ScamAlert from './keyline/ScamAlert.js'
+import SecureDevice from './keyline/SecureDevice.js'
+import Security from './keyline/Security.js'
+import Session from './keyline/Session.js'
+import Sign from './keyline/Sign.js'
+import SmartContract from './keyline/SmartContract.js'
+import Specialised from './keyline/Specialised.js'
+import Storage from './keyline/Storage.js'
+import Success from './keyline/Success.js'
+import Talent from './keyline/Talent.js'
+import Teleport from './keyline/Teleport.js'
+import Time from './keyline/Time.js'
+import Tools from './keyline/Tools.js'
+import Transaction from './keyline/Transaction.js'
+import Transparency from './keyline/Transparency.js'
+import Treasury from './keyline/Treasury.js'
+import Ui from './keyline/Ui.js'
+import Ux from './keyline/Ux.js'
+import Users from './keyline/Users.js'
+import Utility from './keyline/Utility.js'
+import VerifiedAccount from './keyline/VerifiedAccount.js'
+import Verify from './keyline/Verify.js'
+import VideoLayered from './keyline/VideoLayered.js'
+import VideoSingle from './keyline/VideoSingle.js'
+import Vote from './keyline/Vote.js'
+import Wallet from './keyline/Wallet.js'
+import Web3 from './keyline/Web3.js'
+import WhitepaperDownload from './keyline/WhitepaperDownload.js'
+import WhitepaperUpload from './keyline/WhitepaperUpload.js'
+import Whitepaper from './keyline/Whitepaper.js'
+import Wiki from './keyline/Wiki.js'
+
+import SolidAdd from './solid/Add.js'
+import SolidAlert from './solid/Alert.js'
+import SolidAlgorithm from './solid/Algorithm.js'
+import SolidAmbassadors from './solid/Ambassadors.js'
+import SolidArchiveNode from './solid/ArchiveNode.js'
+import SolidArticle from './solid/Article.js'
+import SolidAuctions from './solid/Auctions.js'
+import SolidAuthority from './solid/Authority.js'
+import SolidBackUp from './solid/BackUp.js'
+import SolidBatchTransaction from './solid/BatchTransaction.js'
+import SolidBlockchain from './solid/Blockchain.js'
+import SolidBlogPost from './solid/BlogPost.js'
+import SolidBootNode from './solid/BootNode.js'
+import SolidBridge from './solid/Bridge.js'
+import SolidBuild from './solid/Build.js'
+import SolidCex from './solid/Cex.js'
+import SolidCareer from './solid/Career.js'
+import SolidCode from './solid/Code.js'
+import SolidCommunity from './solid/Community.js'
+import SolidConnectionViaLightNode from './solid/ConnectionViaLightNode.js'
+import SolidConnectionViaRemoteNode from './solid/ConnectionViaRemoteNode.js'
+import SolidConsensus from './solid/Consensus.js'
+import SolidContract from './solid/Contract.js'
+import SolidCopy from './solid/Copy.js'
+import SolidCryptocurrency from './solid/Cryptocurrency.js'
+import SolidDex from './solid/Dex.js'
+import SolidDeFi from './solid/DeFi.js'
+import SolidEcosystem from './solid/Ecosystem.js'
+import SolidEncryption from './solid/Encryption.js'
+import SolidEvents from './solid/Events.js'
+import SolidFailure from './solid/Failure.js'
+import SolidFullNode from './solid/FullNode.js'
+import SolidGaming from './solid/Gaming.js'
+import SolidGift from './solid/Gift.js'
+import SolidGlossaryV1 from './solid/GlossaryV1.js'
+import SolidGlossaryV2 from './solid/GlossaryV2.js'
+import SolidGovernance from './solid/Governance.js'
+import SolidGovernments from './solid/Governments.js'
+import SolidHackathon from './solid/Hackathon.js'
+import SolidInfrastructure from './solid/Infrastructure.js'
+import SolidInterconnected from './solid/Interconnected.js'
+import SolidInvestors from './solid/Investors.js'
+import SolidLayer from './solid/Layer.js'
+import SolidLayerFour from './solid/LayerFour.js'
+import SolidLayerOne from './solid/LayerOne.js'
+import SolidLayerThree from './solid/LayerThree.js'
+import SolidLayerTwo from './solid/LayerTwo.js'
+import SolidLearn from './solid/Learn.js'
+import SolidLibrary from './solid/Library.js'
+import SolidLightNode from './solid/LightNode.js'
+import SolidMaintain from './solid/Maintain.js'
+import SolidMessage from './solid/Message.js'
+import SolidMetaverse from './solid/Metaverse.js'
+import SolidMonitor from './solid/Monitor.js'
+import SolidMultisign from './solid/Multisign.js'
+import SolidNft from './solid/Nft.js'
+import SolidNode from './solid/Node.js'
+import SolidNominating from './solid/Nominating.js'
+import SolidOracleV1 from './solid/OracleV1.js'
+import SolidOracleV2 from './solid/OracleV2.js'
+import SolidPicture from './solid/Picture.js'
+import SolidPodcasts from './solid/Podcasts.js'
+import SolidPrivateKey from './solid/PrivateKey.js'
+import SolidProofOfStake from './solid/ProofOfStake.js'
+import SolidProofOfWork from './solid/ProofOfWork.js'
+import SolidProposal from './solid/Proposal.js'
+import SolidProxy from './solid/Proxy.js'
+import SolidPublicKey from './solid/PublicKey.js'
+import SolidRandomness from './solid/Randomness.js'
+import SolidRecovery from './solid/Recovery.js'
+import SolidRemoteNode from './solid/RemoteNode.js'
+import SolidRemove from './solid/Remove.js'
+import SolidReport from './solid/Report.js'
+import SolidScamAlert from './solid/ScamAlert.js'
+import SolidSecureDevice from './solid/SecureDevice.js'
+import SolidSecurity from './solid/Security.js'
+import SolidSession from './solid/Session.js'
+import SolidSign from './solid/Sign.js'
+import SolidSmartContract from './solid/SmartContract.js'
+import SolidSpecialised from './solid/Specialised.js'
+import SolidStorage from './solid/Storage.js'
+import SolidSuccess from './solid/Success.js'
+import SolidTalent from './solid/Talent.js'
+import SolidTeleport from './solid/Teleport.js'
+import SolidTime from './solid/Time.js'
+import SolidTools from './solid/Tools.js'
+import SolidTransaction from './solid/Transaction.js'
+import SolidTransparency from './solid/Transparency.js'
+import SolidTreasury from './solid/Treasury.js'
+import SolidUi from './solid/Ui.js'
+import SolidUx from './solid/Ux.js'
+import SolidUsers from './solid/Users.js'
+import SolidUtility from './solid/Utility.js'
+import SolidVerifiedAccount from './solid/VerifiedAccount.js'
+import SolidVerify from './solid/Verify.js'
+import SolidVideoLayered from './solid/VideoLayered.js'
+import SolidVote from './solid/Vote.js'
+import SolidWallet from './solid/Wallet.js'
+import SolidWeb3 from './solid/Web3.js'
+import SolidWhitepaperDownload from './solid/WhitepaperDownload.js'
+import SolidWhitepaperUpload from './solid/WhitepaperUpload.js'
+import SolidWhitepaper from './solid/Whitepaper.js'
+import SolidWiki from './solid/Wiki.js'
+
+const keylineIcons = {
+	Add,
+	Alert,
+	Algorithm,
+	Ambassadors,
+	ArchiveNode,
+	Article,
+	Auctions,
+	Authority,
+	BackUp,
+	BatchTransaction,
+	Blockchain,
+	BlogPost,
+	BootNode,
+	Bridge,
+	Build,
+	Cex,
+	Career,
+	Code,
+	Community,
+	ConnectionViaLightNode,
+	ConnectionViaRemoteNode,
+	Consensus,
+	Contract,
+	Copy,
+	Cryptocurrency,
+	Dex,
+	DeFi,
+	Ecosystem,
+	Encryption,
+	Events,
+	Failure,
+	FullNode,
+	Gaming,
+	Gift,
+	GlossaryV1,
+	GlossaryV2,
+	Governance,
+	Governments,
+	Hackathon,
+	Infrastructure,
+	Interconnected,
+	Investors,
+	Layer,
+	LayerFour,
+	LayerOne,
+	LayerThree,
+	LayerTwo,
+	Learn,
+	Library,
+	LightNode,
+	Maintain,
+	Message,
+	Metaverse,
+	Monitor,
+	Multisign,
+	Nft,
+	Node,
+	Nominating,
+	OracleV1,
+	OracleV2,
+	Picture,
+	Podcasts,
+	PrivateKey,
+	ProofOfStake,
+	ProofOfWork,
+	Proposal,
+	Proxy,
+	PublicKey,
+	Randomness,
+	Recovery,
+	RemoteNode,
+	Remove,
+	Report,
+	ScamAlert,
+	SecureDevice,
+	Security,
+	Session,
+	Sign,
+	SmartContract,
+	Specialised,
+	Storage,
+	Success,
+	Talent,
+	Teleport,
+	Time,
+	Tools,
+	Transaction,
+	Transparency,
+	Treasury,
+	Ui,
+	Ux,
+	Users,
+	Utility,
+	VerifiedAccount,
+	Verify,
+	VideoLayered,
+	Vote,
+	Wallet,
+	Web3,
+	WhitepaperDownload,
+	WhitepaperUpload,
+	Whitepaper,
+	Wiki,
+}
+const solidIcons = {
+	SolidAdd,
+	SolidAlert,
+	SolidAlgorithm,
+	SolidAmbassadors,
+	SolidArchiveNode,
+	SolidArticle,
+	SolidAuctions,
+	SolidAuthority,
+	SolidBackUp,
+	SolidBatchTransaction,
+	SolidBlockchain,
+	SolidBlogPost,
+	SolidBootNode,
+	SolidBridge,
+	SolidBuild,
+	SolidCex,
+	SolidCareer,
+	SolidCode,
+	SolidCommunity,
+	SolidConnectionViaLightNode,
+	SolidConnectionViaRemoteNode,
+	SolidConsensus,
+	SolidContract,
+	SolidCopy,
+	SolidCryptocurrency,
+	SolidDex,
+	SolidDeFi,
+	SolidEcosystem,
+	SolidEncryption,
+	SolidEvents,
+	SolidFailure,
+	SolidFullNode,
+	SolidGaming,
+	SolidGift,
+	SolidGlossaryV1,
+	SolidGlossaryV2,
+	SolidGovernance,
+	SolidGovernments,
+	SolidHackathon,
+	SolidInfrastructure,
+	SolidInterconnected,
+	SolidInvestors,
+	SolidLayer,
+	SolidLayerFour,
+	SolidLayerOne,
+	SolidLayerThree,
+	SolidLayerTwo,
+	SolidLearn,
+	SolidLibrary,
+	SolidLightNode,
+	SolidMaintain,
+	SolidMessage,
+	SolidMetaverse,
+	SolidMonitor,
+	SolidMultisign,
+	SolidNft,
+	SolidNode,
+	SolidNominating,
+	SolidOracleV1,
+	SolidOracleV2,
+	SolidPicture,
+	SolidPodcasts,
+	SolidPrivateKey,
+	SolidProofOfStake,
+	SolidProofOfWork,
+	SolidProposal,
+	SolidProxy,
+	SolidPublicKey,
+	SolidRandomness,
+	SolidRecovery,
+	SolidRemoteNode,
+	SolidRemove,
+	SolidReport,
+	SolidScamAlert,
+	SolidSecureDevice,
+	SolidSecurity,
+	SolidSession,
+	SolidSign,
+	SolidSmartContract,
+	SolidSpecialised,
+	SolidStorage,
+	SolidSuccess,
+	SolidTalent,
+	SolidTeleport,
+	SolidTime,
+	SolidTools,
+	SolidTransaction,
+	SolidTransparency,
+	SolidTreasury,
+	SolidUi,
+	SolidUx,
+	SolidUsers,
+	SolidUtility,
+	SolidVerifiedAccount,
+	SolidVerify,
+	SolidVideoLayered,
+	SolidVote,
+	SolidWallet,
+	SolidWeb3,
+	SolidWhitepaperDownload,
+	SolidWhitepaperUpload,
+	SolidWhitepaper,
+	SolidWiki,
+}
+
+// export { default as keylineIcons } from './keyline/keyline/index.js'
+// export { default as solidIcons } from './keyline/solid/index.js'
+
+// import solidIcons from './keyline/solid/index.js'
+
+export const icons = {
+	keyline: keylineIcons,
+	solid: solidIcons,
+}
