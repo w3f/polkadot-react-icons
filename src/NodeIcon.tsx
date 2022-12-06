b1d92913541b7384494e91895e675a4a2eef0988
Nikki Strømsnes <hello@tinybits.dk>
2022-12-06 17:00:07 +0100

    Split keyline and solid icons in export

--- src/NodeIcon.tsx
+++
@@ -1,7 +0,0 @@
-///<reference path="./types/svg.d.ts" />
-import React from 'react'
-import Node from './icons/Node.svg'
-
-export const NodeIcon = () => {
-	return <div dangerouslySetInnerHTML={{ __html: Node.toString() }} />
-}
