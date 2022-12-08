0012940a2b064636cf23e1264e7466e4c877171a
Nikki Strømsnes <hello@tinybits.dk>
2022-12-08 10:41:30 +0100

    Update some B1 icons for comparision

---
+++ src/keyline/ContractVector.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const ContractVector = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M8.99414 9V2H25.9941V19H19.4941M1.99414 9H18.9941V26H1.99414V9Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+export default ContractVector;
---
+++ src/keyline/CopyNew.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { SVGProps } from "react";
+const CopyNew = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M8.99414 9V2H25.9941V19H19.4941" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M18.9941 9H1.99414V26H18.9941V9Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+export default CopyNew;
