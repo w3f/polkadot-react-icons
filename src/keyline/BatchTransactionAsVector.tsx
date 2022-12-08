f972642f675be590818272d4128ed429a8ac4214
Nikki Strømsnes <hello@tinybits.dk>
2022-12-08 14:40:24 +0100

    Update all batch 1 keyline

--- src/keyline/BatchTransactionAsVector.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { SVGProps } from "react";
-const BatchTransactionAsVector = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M25.046 16.5139L15.4727 26M11.5733 3.13323L2 12.6193M25.6152 2.38659L2.10723 26M20.4477 15.3989L25.9818 15.3807L26 20.9302M6.97498 2.01823L12.5091 2L12.5273 7.54949M20.4477 2.01823L25.9818 2L26 7.54949" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
-export default BatchTransactionAsVector;
--- src/keyline/Transaction.tsx
+++ src/keyline/Transaction.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { SVGProps } from "react";
-const Transaction = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={26} viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M26 7.47375L1.99998 7.47375M26 7.47375L20.4786 1.95249M26 7.47375L20.4786 13.0045" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M1.99998 18.5213H25.9918M1.99998 18.5213L7.52136 24.0521M1.99998 18.5213L7.52136 13.0001" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const Transaction = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={26} viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M26 7.47377L2 7.47377M26 7.47377L20.4787 1.95251M26 7.47377L20.4787 13.0045M2 18.5213H25.9918M2 18.5213L7.52138 24.0521M2 18.5213L7.52138 13.0001" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
 export default Transaction;
