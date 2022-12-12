f42d34412a5d912bddbcec09404b173574ac16bc
Nikki Strømsnes <hello@tinybits.dk>
2022-12-12 13:54:42 +0100

    Add no-fill flags for keyline icons

--- src/keyline/ArrowRight.tsx
+++ src/keyline/ArrowRight.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { SVGProps } from "react";
-const ArrowRight = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={24} viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M25 12.0086L2 12.0086" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M15.8003 2L26 12.0086L15.8003 22" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const ArrowRight = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={24} viewBox="0 0 28 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M25 12.0086L2 12.0086" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" data-nofill="true" /><path d="M15.8003 2L26 12.0086L15.8003 22" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" data-nofill="true" /></svg>;
 export default ArrowRight;
