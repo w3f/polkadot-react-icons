c2b09f3fd7ee9eb6422685156b36bfec17ad0d46
Nikki Strømsnes <hello@tinybits.dk>
2022-12-12 13:14:11 +0100

    New keyline revisions

--- src/keyline/Career.tsx
+++ src/keyline/Career.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { SVGProps } from "react";
-const Career = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={25} viewBox="0 0 28 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M26 6.62796H2V23.2377H26V6.62796Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M9.38129 6.45019V2H18.6192V6.5" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M3 13.3445L25 13.3445" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const Career = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={25} viewBox="0 0 28 25" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M9.38129 6.45019V2H18.6192V6.5" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M26 6.62796H2V23.2377H26V6.62796Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M3 13.3445L25 13.3445" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
 export default Career;
--- src/keyline/Lock.tsx
+++ src/keyline/Lock.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { SVGProps } from "react";
-const Lock = (props: SVGProps<SVGSVGElement>) => <svg width={25} height={27} viewBox="0 0 25 27" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M23.0798 9.66925H2V25H23.0798V9.66925Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M7.12132 9.15414L7.12134 7.41857V7.41857C7.12134 4.42598 9.54731 2 12.5399 2V2C15.5325 2 17.9585 4.42598 17.9585 7.41857V7.41857L17.9585 9.15414" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
+const Lock = (props: SVGProps<SVGSVGElement>) => <svg width={25} height={27} viewBox="0 0 25 27" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M7.12132 9.15414L7.12134 7.41857V7.41857C7.12134 4.42598 9.54731 2 12.5399 2V2C15.5325 2 17.9585 4.42598 17.9585 7.41857V7.41857L17.9585 9.15414" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M23.0798 9.66925H2V25H23.0798V9.66925Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /></svg>;
 export default Lock;
