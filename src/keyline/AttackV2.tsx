e1b36d8fead5a20ab730d0ce95bf42ca923dd0c4
Nikki Strømsnes <hello@tinybits.dk>
2022-12-20 13:18:07 +0100

    Fix naming and delete dupliates of some icons

--- src/keyline/AttackV2.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { SVGProps } from "react";
-const AttackV2 = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M14 2C7.37258 2 2 7.37258 2 14C2 16.1857 2.58437 18.235 3.60539 20H7.32682V26H20.6602V20H24.3946C25.4156 18.235 26 16.1857 26 14C26 7.37258 20.6274 2 14 2Z" stroke="black" strokeWidth={2.5} strokeLinejoin="round" /><circle cx={9.28605} cy={13.0698} r={1.88557} stroke="black" strokeWidth={2.5} /><circle cx={18.7139} cy={13.0698} r={1.88557} stroke="black" strokeWidth={2.5} /></svg>;
-export default AttackV2;
--- src/keyline/BringingExternalTokensIntoPolkadot.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { SVGProps } from "react";
-const BringingExternalTokensIntoPolkadot = (props: SVGProps<SVGSVGElement>) => <svg width={28} height={20} viewBox="0 0 28 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M10.0002 1.90186H26.0002V17.9019H10.0002" stroke="black" strokeWidth={2.5} strokeLinejoin="round" /><circle cx={10} cy={9.90173} r={8} stroke="black" strokeWidth={2.5} /><path d="M10.7999 9.90161H17.1999" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M10.7999 5.90186H16.3999" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M10.7999 13.9017H16.3999" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M10.0002 1.90186V17.9019" stroke="black" strokeWidth={2.5} strokeLinejoin="round" /></svg>;
-export default BringingExternalTokensIntoPolkadot;
