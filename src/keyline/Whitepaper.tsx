cc62d170fe8ba17faa6fbb8c4124ba19e1bc5472
Nikki Strømsnes <hello@tinybits.dk>
2022-12-20 12:25:22 +0100

    Update keyline icons

--- src/keyline/Whitepaper.tsx
+++ src/keyline/Whitepaper.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 import { SVGProps } from "react";
-const Whitepaper = (props: SVGProps<SVGSVGElement>) => <svg width={22} height={28} viewBox="0 0 22 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M2.00001 26V2H14L20.4615 8.46154V26H2.00001Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M19.4305 8.46152H14.0001V3.41327" stroke="black" strokeWidth={2.5} strokeLinecap="square" strokeLinejoin="round" /></svg>;
+const Whitepaper = (props: SVGProps<SVGSVGElement>) => <svg width={22} height={28} viewBox="0 0 22 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path d="M2.00001 26V2H14L20.4615 8.46154V26H2.00001Z" stroke="black" strokeWidth={2.5} strokeLinecap="round" strokeLinejoin="round" /><path d="M19.5 8.46152H14.0001V3" stroke="black" strokeWidth={2.5} strokeLinecap="square" strokeLinejoin="round" /></svg>;
 export default Whitepaper;
