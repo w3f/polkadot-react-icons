f456809b1e833c52d4f0d767b06f92c6d7fee638
Nikki Strømsnes <hello@tinybits.dk>
2023-01-26 15:54:30 +0100

    Do some minor icon adjustments

--- src/keyline/DataAndAnalytics.tsx
+++ src/keyline/DataAndAnalytics.tsx
@@ -12,18 +12,15 @@ const DataAndAnalytics = (props: SVGProps<SVGSVGElement>) => (
 		strokeLinejoin="round"
 		{...props}
 	>
-		<path d="M5.00616 11.1631L11.3374 4.83182" fill="none" data-nofill="true" />
-		<path d="M17.6687 11.1631L11.3374 4.83182" fill="none" data-nofill="true" />
-		<path d="M17.6687 11.1631L24 4.83182" fill="none" data-nofill="true" />
-		<path d="M11.3313 11.3865L11.3313 23.2954" fill="none" data-nofill="true" />
-		<path d="M5 15.6073L5 23.2953" fill="none" data-nofill="true" />
-		<path d="M17.6625 15.6073L17.6625 23.2954" fill="none" data-nofill="true" />
-		<path d="M23.9938 12.4417L23.9938 23.2953" fill="none" data-nofill="true" />
 		<path
-			d="M19.7792 3.99998L23.9938 3.99998L23.9938 8.22083"
+			d="M5.00616 11.1632L11.3374 4.83194L17.6687 11.1632L23.303 4.55295"
 			fill="none"
-			data-nofill="true"
 		/>
+		<path d="M11.3313 11.3865L11.3313 23.2954" fill="none" />
+		<path d="M5 15.6073L5 23.2953" fill="none" />
+		<path d="M17.6625 15.6073L17.6625 23.2954" fill="none" />
+		<path d="M23.9938 12.4418L23.9938 23.2954" fill="none" />
+		<path d="M19.7792 3.99998L23.9938 3.99998L23.9938 8.22083" fill="none" />
 	</svg>
 )
 export default DataAndAnalytics
